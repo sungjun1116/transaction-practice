2399250444dc9711032524092253bcf758c56aee
sungjun1116 <opr99@naver.com>
2026-03-05 21:22:06 +0900

    feat: 테이블명 수정

--- src/main/java/com/hanwha/transactionpractice/practice1/AuditLog.java
+++ src/main/java/com/hanwha/transactionpractice/practice1/AuditLog.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "p1_audit_log")
+@Table(name = "audit_log")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuditLog {
--- src/main/java/com/hanwha/transactionpractice/practice1/Order.java
+++ src/main/java/com/hanwha/transactionpractice/practice1/Order.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "p1_orders")
+@Table(name = "orders")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
